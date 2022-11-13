ab50c4ca83afc4ad2467449a63a881e63c7c02b7
Kander Akinci <87019962+AI-Assistant@users.noreply.github.com>
2022-11-13 16:00:47 +0100

    Update Program.cs

--- Aufgabe1_Kleinere_Zahl/Program.cs
+++ Aufgabe1_Kleinere_Zahl/Program.cs
@@ -3,7 +3,7 @@
 
 /// <summary>
 /// SAS Kontrollstrukturen
-/// Arbeitsauftrag 1 - Aufgabe 1
+/// Arbeitsauftrag 3 - Aufgabe 1
 /// Die kleinere Zahl
 /// </summary>
 
@@ -16,25 +16,6 @@ public static class Aufgabe_1
     {
 
         //Diese Zeile mit Ihrem Quellcode ersetzen
-
-        double z1,z2;
-        string erg;
-        Console.WriteLine("Die kleinere Zahl\n");
-        
-        Console.WriteLine("Geben Sie die erste Zahl ein:");
-        double.TryParse(Console.ReadLine(), out z1); 
-
-        Console.WriteLine("Geben Sie die zweite Zahl ein:");
-        double.TryParse(Console.ReadLine(), out z2);
-
-        Console.WriteLine("------------------");
-
-        if (z1 == z2) erg = "Gleicher Wert";
-        else if (z1 < z2) erg = z1.ToString();
-        else erg =z2.ToString();
-
-        Console.WriteLine($"Die kleinere Zahl ist: {erg}");
-
 
     }
 }
