dc8c4afd31d728b00c9bbec9a1418764f9ca065e
Kander Akinci <87019962+AI-Assistant@users.noreply.github.com>
2022-11-13 16:01:45 +0100

    Update Program.cs

--- Aufgabe3_Rechner/Program.cs
+++ Aufgabe3_Rechner/Program.cs
@@ -3,8 +3,8 @@
 
 /// <summary>
 /// SAS Kontrollstrukturen
-/// Arbeitsauftrag 1 - Aufgabe3
-/// Reisezeit Rechner
+/// Arbeitsauftrag 3 - Aufgabe3
+/// Rechner
 /// </summary>
 
 
@@ -14,51 +14,8 @@ public static class Aufgabe_3
 {
     public static void Aufgabe3()
     {
-        double w1,w2;
-        int ausw;
-        string erg;
 
-        Console.WriteLine("Rechner\n");
-        Console.WriteLine("Geben Sie den ersten Wert ein:");
-        double.TryParse(Console.ReadLine(), out w1);
-
-        Console.WriteLine("Geben Sie den zweiten Wert ein:");
-        double.TryParse(Console.ReadLine(), out w2);
-
-        Console.WriteLine("1 Addition");
-        Console.WriteLine("2 Subraktion");
-        Console.WriteLine("3 Multiplikation");
-        Console.WriteLine("4 Divison");
-        Console.WriteLine("5 Potenz");
-        Console.WriteLine("Wählen Sie einen Operator:");
-        int.TryParse(Console.ReadLine(), out ausw);
-
-
-        switch (ausw)
-        {
-            case 1:
-                erg =(w1 + w2).ToString();
-                break;
-
-            case 2:
-                erg = (w1 - w2).ToString();
-                break;
-            case 3:
-                erg = (w1 * w2).ToString();
-                break;
-            case 4:
-                erg = (w1 / w2).ToString();
-                break;
-            case 5:
-                erg = Math.Pow(w1, w2).ToString();
-                break;
-            default:
-                erg = "Error";
-                break;
-        }
-
-        Console.WriteLine("------------------");
-        Console.WriteLine($"Ihr Ergebnis ist: {erg}");
+        //Diese Zeile mit Ihrem Quellcode ersetzen
 
     }
 }
